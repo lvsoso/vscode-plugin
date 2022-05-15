f057d80f70c2bab5fbe850a2d16d7a17ff20a1e7
lvsoso <wuqize5109@qq.com>
2022-05-15 19:48:15 +0800

    webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,40 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorldPanel";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vstodo" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "vstodo" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vstodo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSTodo!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vstodo.helloWorld', () => {
+      HelloWorldPanel.createOrShow(context.extensionUri );
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vstodo.addTodo', async () => {
+      const answer = await vscode.window.showInformationMessage(
+        'add todo to VSTodo!',
+        'y',
+        'n'
+      );
+
+	  if (answer === 'n') {
+		  vscode.window.showInformationMessage('ok');
+	  } else {
+		  console.log(answer);
+	  }
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
